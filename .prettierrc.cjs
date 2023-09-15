040d7553f125504c7e3ca2b53e776517e04f1b4b
shizhiheng <2225018807@qq.com>
2023-09-15 11:25:21 +0800

    prettier和eslint格式代码

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80, //换货
+  tabWidth: 2, //缩进2个空格
+  useTabs: false, //不是tab缩进
+  semi: false, //一行代码最后的分号 ;
+  singleQuote: true, //单引号
+  bracketSpacing: true, //内容间距，如{ name }
+  bracketSameLine: false, //如：/>不放在同一行
+}
--- src/views/page1/Page1.tsx
+++ src/views/page1/Page1.tsx
@@ -1,17 +1,15 @@
 import { Button } from 'antd'
-import {useSelector,useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import counterStore from '@/store/counterReducer/index.js'
 // import {RootState} from '@/types/store.js'
 
 export default function Page1() {
-  const dispatch=useDispatch()
-  const {num,arr}=useSelector((state:RootState)=>(
-    {
-      num:state.counterReducer.num,
-      arr:state.arrReducer.arr
-    }
-  ))
-  const handleClick=()=>{
+  const dispatch = useDispatch()
+  const { num, arr } = useSelector((state: RootState) => ({
+    num: state.counterReducer.num,
+    arr: state.arrReducer.arr,
+  }))
+  const handleClick = () => {
     // 同步
     // dispatch({type:'add1'})
     // 异步 redux-thunk的用法格式：dispath(异步执行函数)
@@ -25,19 +23,22 @@ export default function Page1() {
     dispatch(counterStore.asyncAction.asyncAdd1)
     // dispatch({type:'add2',val:10})
   }
-  const handleClickArr=()=>{
-    dispatch({type:'addArr',val:'你好---'})
+  const handleClickArr = () => {
+    dispatch({ type: 'addArr', val: '你好---' })
     // dispatch({type:'add2',val:10})
     console.log(arr)
   }
   return (
     <div>
-       <div>初始值为{num}</div>
-       <Button type='primary' onClick={handleClick}>点击add2</Button>
-       <div>{arr}</div>
-       <br />
-       <Button type='primary' onClick={handleClickArr}>点击addArr</Button>
-
+      <div>初始值为{num}</div>
+      <Button type="primary" onClick={handleClick}>
+        点击add2
+      </Button>
+      <div>{arr}</div>
+      <br />
+      <Button type="primary" onClick={handleClickArr}>
+        点击addArr
+      </Button>
     </div>
   )
 }
